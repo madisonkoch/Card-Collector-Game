cf4acee318ee0301f6c6c4b892a1fc002a87eaba
madisonkoch <kochx348@umn.edu>
2018-03-11 17:55:51 -0500

    Game & Aesthetic done - still need to check submission requirements

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,19 @@
 'use strict'
 
 //Variables
-    const envelopeOne = Math.floor(Math.random()*12)+1;
-    const envelopeTwo = Math.floor(Math.random()*12)+1;
-    const envelopeThree = Math.floor(Math.random()*12)+1;
-    const envelopeFour = Math.floor(Math.random()*12)+1;
+    let envelopeOne = Math.floor(Math.random()*12)+1;
+    let envelopeTwo = Math.floor(Math.random()*12)+1;
+    let envelopeThree = Math.floor(Math.random()*12)+1;
+    let envelopeFour = Math.floor(Math.random()*12)+1;
     const goalNumberFunction = function getRandomIntInclusive(goalMin, goalMax) {
         goalMin = Math.ceil(19);
         goalMax = Math.floor(120);
         return Math.floor(Math.random() * (goalMax - goalMin + 1)) + goalMin;
       }
-    const goalNumber = goalNumberFunction();
+    let goalNumber = goalNumberFunction();
     let envelopeClickedArray = [];
     let score = 0;
+    let runningScore = 0;
     let wins = 0;
     let losses = 0;
 //
@@ -21,16 +22,28 @@
      console.log(score);
      console.log(goalNumber);
 //
-//Clear Function (partial for next game)
+//NextGame Function (partial for next game)
     const nextGame = function(){
-        envelopeOne = "";
-        envelopeTwo = "";
-        envelopeThree = "";
-        envelopeFour = "";
-        goalNumber = "";
-        envelopeValue = "";
+        envelopeOne = Math.floor(Math.random()*12)+1;
+        envelopeTwo = Math.floor(Math.random()*12)+1;
+        envelopeThree = Math.floor(Math.random()*12)+1;
+        envelopeFour = Math.floor(Math.random()*12)+1;
+
+        goalNumber = goalNumberFunction();
+
         score = 0;
-        //score on dom
+        runningScore = 0;
+
+        $('#envelopeOne').val(envelopeOne);
+        $('#envelopeTwo').val(envelopeTwo);
+        $('#envelopeThree').val(envelopeThree);
+        $('#envelopeFour').val(envelopeFour);
+        
+        $('#goalNumber').text(goalNumber);
+
+        $('#score').text(score);
+
+        console.log(envelopeOne, envelopeTwo, envelopeThree, envelopeFour);
     }
 //
 //Initial Game State
@@ -43,26 +56,40 @@
     $('#wins').text(wins);
     $('#losses').text(losses);
     $('#score').text(score);
-
+//
 //Game
-$('.envelope').click(function(){
-    let envelopeValue = $(this).val();
-    envelopeClickedArray.push(parseInt(envelopeValue));
-    score = envelopeClickedArray.reduce(function(score, elem){
-        return score + elem;
-    }, 0);
-    console.log(goalNumber, score);
-    $('#score').text(score);
-    winLoss();
-});
+    $('.envelope').click(function(){
+        let envelopeValue = $(this).val();
+        envelopeClickedArray.push(parseInt(envelopeValue));
+        runningScore = envelopeClickedArray.reduce(function(score, elem){
+            return score + elem;
+        }, 0);
+        console.log(goalNumber, runningScore);
+        $('#score').text(runningScore);
+        winLoss();
+    });
+//
+//WinLoss
+    const winLoss = function(){
+        if (runningScore === goalNumber){
+            wins++;
+            $('#wins').text(wins);
+            document.getElementById('winner').style.visibility = "visible";
+            nextGame();
+        }
+        else if (runningScore > goalNumber){
+            losses++;
+            $('#losses').text(losses);
+            document.getElementById('loser').style.visibility = "visible";
+            nextGame();
+        }
+    }
+//
+//Style
+    $('#instructions').click(function(){
+        document.getElementById('instructions').style.visibility = "hidden";
+    });
 
-const winLoss = function(){
-    if (score === goalNumber){
-        wins++;
-        $('#wins').text(wins);
-    }
-    else if (score > goalNumber){
-        losses++;
-        $('#losses').text(losses);
-    }
-}
+    $('#inst-img').click(function(){
+        document.getElementById('instructions').style.visibility = "visible"; 
+    });
